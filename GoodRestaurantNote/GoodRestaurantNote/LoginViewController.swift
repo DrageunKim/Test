6d312d6a61e304dfb70e437a15b32b931fbdf00a
DragonYG <kioswe92@gmail.com>
2023-02-14 22:29:01 +0900

    feat: Login화면 UI 기본 구성

---
+++ GoodRestaurantNote/GoodRestaurantNote/LoginViewController.swift
@@ -0,0 +1,181 @@
+//
+//  LoginViewController.swift
+//  GoodRestaurantNote
+//
+//  Created by yonggeun Kim on 2023/02/14.
+//
+
+import UIKit
+import FacebookLogin
+import GoogleSignIn
+import KakaoSDKUser
+
+class LoginViewController: UIViewController {
+
+    // MARK: Private Properties
+    
+    private let introduceTopLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .headline).withSize(20)
+        label.text = "   맛집노트"
+        label.textAlignment = .left
+        label.textColor = .label
+        return label
+    }()
+    private let introduceBottomLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .headline)
+        label.text = "   123"
+        label.textAlignment = .left
+        label.textColor = .brown
+        return label
+    }()
+    private let nonMemberLoginButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .label
+        button.setTitle("비회원 로그인", for: .normal)
+        button.setTitleColor(UIColor.systemBackground, for: .normal)
+        button.layer.cornerRadius = 5
+        return button
+    }()
+    private let kakaoLoginButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "KakaoLogin.png"), for: .normal)
+        return button
+    }()
+    private let facebookLoginButton: FBLoginButton = {
+        let button = FBLoginButton()
+        return button
+    }()
+    private let googleLoginButton: GIDSignInButton = {
+        let button = GIDSignInButton()
+        button.style = .wide
+        return button
+    }()
+    private let introduceStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.alignment = .leading
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.backgroundColor = .systemGray4
+        stackView.layer.cornerRadius = 10
+        stackView.layer.borderColor = UIColor.label.cgColor
+        stackView.layer.borderWidth = 2
+        return stackView
+    }()
+    private let loginStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.alignment = .fill
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.spacing = 20
+        return stackView
+    }()
+    
+    // MARK: Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureView()
+        configureLayout()
+        configureButton()
+    }
+    
+    // MARK: Private Methods
+    
+    private func configureView() {
+        view.backgroundColor = .systemBackground
+        
+        navigationItem.title = "계정 로그인"
+    }
+    
+    private func configureButton() {
+        googleLoginButton.style = .wide
+        
+        googleLoginButton.addTarget(self, action: #selector(tappedGoogleLoginButton), for: .touchDown)
+        kakaoLoginButton.addTarget(self, action: #selector(tappedKakaoLoginButton), for: .touchDown)
+    }
+    
+    private func setUpStackView() {
+        introduceStackView.addArrangedSubview(introduceTopLabel)
+        introduceStackView.addArrangedSubview(introduceBottomLabel)
+        
+        loginStackView.addArrangedSubview(nonMemberLoginButton)
+        loginStackView.addArrangedSubview(kakaoLoginButton)
+        loginStackView.addArrangedSubview(facebookLoginButton)
+        loginStackView.addArrangedSubview(googleLoginButton)
+    }
+    
+    private func configureLayout() {
+        setUpStackView()
+        
+        view.addSubview(introduceStackView)
+        view.addSubview(loginStackView)
+        
+        NSLayoutConstraint.activate([
+            introduceStackView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5),
+            introduceStackView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.09),
+            introduceStackView.topAnchor.constraint(
+                equalTo: view.topAnchor,
+                constant: view.frame.size.height * 0.2
+            ),
+            introduceStackView.leadingAnchor.constraint(
+                equalTo: view.leadingAnchor,
+                constant: view.frame.size.width * 0.1
+            ),
+            
+            loginStackView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8),
+            loginStackView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.32),
+            loginStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            loginStackView.bottomAnchor.constraint(
+                equalTo: view.bottomAnchor,
+                constant: view.frame.size.height * -0.1
+            )
+        ])
+    }
+}
+
+// MARK: - Action Methods
+
+extension LoginViewController {
+    @objc
+    private func tappedKakaoLoginButton() {
+        if (UserApi.isKakaoTalkLoginAvailable()) {
+            UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
+                if let error = error {
+                    print(error)
+                }
+                else {
+                    print("loginWithKakaoTalk() success.")
+
+                    //do something
+                    _ = oauthToken
+                }
+            }
+        } else {
+            UserApi.shared.loginWithKakaoAccount {(oauthToken, error) in
+                if let error = error {
+                    print(error)
+                }
+                else {
+                    print("loginWithKakaoAccount() success.")
+                    
+                    //do something
+                    _ = oauthToken
+                }
+            }
+        }
+    }
+    
+    @objc
+    private func tappedGoogleLoginButton() {
+        GIDSignIn.sharedInstance.signIn(withPresenting: self) { signInResult, error in
+            guard error == nil else { return }
+            
+            // If sign in succeeded, display the app's main content View.
+        }
+    }
+}
--- GoodRestaurantNote/GoodRestaurantNote/SceneDelegate.swift
+++ GoodRestaurantNote/GoodRestaurantNote/SceneDelegate.swift
@@ -16,8 +16,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
+        let navigationController = UINavigationController(rootViewController: LoginViewController())
         
-        window.rootViewController = ViewController()
+        window.rootViewController = navigationController
         window.makeKeyAndVisible()
         
         self.window = window
